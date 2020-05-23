cccbd66c1adc566c86c0fa8cca0c82a3e9692bf7
mozanunal <mehmetozanunal@gmail.com>
2020-05-23 17:15:06 +0300

    main class docs added

--- mqtt_decorator/conn.py
+++ mqtt_decorator/conn.py
@@ -22,8 +22,41 @@ class MqttDecorator(object):
 
         Arguments:
             mqtt_client {paho.mqtt.client.Client} -- [description]
+
+        mqtt_decorator is a decorator module which converts 
+        mqtt subscriptions and messages to a Flask like api.
+
+        ```python
+        from mqttdecorator import MqttDecorator
+        import paho.mqtt.client as mqtt
+
+        # you can specify all 
+        # paho mqtt client options
+        # such as websocket connections or
+        # tls connections
+        mqttc = mqtt.Client(clean_session=True)
+        app = MqttDecorator(mqttc)
+
+        @app.route("$SYS/<broker>/<type>")
+        def broker_url_params(msg, broker, type):
+            print("---broker_url_params", msg.topic, msg.payload)
+            print("--broker", broker)
+            print("--type", type)
+
+        @app.route("$SYS/broker/version")
+        def version(msg):
+            print("---version", msg.topic, msg.payload)
+
+        @app.route("$SYS/broker/uptime")
+        def uptime(msg):
+            print("---uptime", msg.topic, msg.payload)
+
+
+        if __name__ == "__main__":
+            app.run( "mqtt.eclipse.org", 1883 )
+
+        ```
         """
-        print(type(mqtt_client))
         # init routes
         self.routes = {}
 
